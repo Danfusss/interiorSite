1c3540c377e5844e7e0839bf4d6a13968b4e3b1b
danfuss <danfokin.20@yandex.ru>
2023-02-08 10:30:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar/NavBar";
+
+export default function App() {
+  return (
+    <body>
+      <Navbar />
+    </body>
+  );
+}
---
+++ src/Navbar/NavBar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import NavbarList from "./NavbarList/NavbarList";
+
+export default function Navbar() {
+  return <NavbarList />;
+}
